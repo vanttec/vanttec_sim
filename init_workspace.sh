3e5d3373e967e45e54929d0659ef714fdbf6fc47
vSebas <sebas.martp@gmail.com>
2020-11-07 16:11:16 -0600

    Updated shell script

--- init_workspace.sh
+++ init_workspace.sh
@@ -5,7 +5,7 @@ mkdir vanttec_uv_sim_ws/
 cd vanttec_uv_sim_ws/
 mkdir src/
 mv ~/vanttec_uv_sim ~/vanttec_uv_sim_ws/src
-cd ./src
+cd ~/vanttec_uv_sim_ws/src
 git submodule update --init --recursive
 cd /vanttec_uv_sim_ws
 catkin_make
