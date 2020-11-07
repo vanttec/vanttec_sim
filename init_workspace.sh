825a00b83f3b971c0c8a3b5dae24c7c9e2debcdd
Sebastian Martínez <55563384+vSebas@users.noreply.github.com>
2020-11-07 16:20:02 -0600

    Update init_workspace.sh

--- init_workspace.sh
+++ init_workspace.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 cd
-mkdir vanttec_uv_sim_ws/
-cd vanttec_uv_sim_ws/
-mkdir src/
-mv ~/vanttec_uv_sim ~/vanttec_uv_sim_ws/src
-cd ~/vanttec_uv_sim_ws/src/vanttec_uv_sim
+mkdir vanttec_uv_sim_ws
+mv vanttec_uv_sim vanttec_uv_sim_ws/src
+cd vanttec_uv_sim_ws/src
 git submodule update --init --recursive
-cd /vanttec_uv_sim_ws
+cd 
+cd vanttec_uv_sim_ws
 catkin_make
