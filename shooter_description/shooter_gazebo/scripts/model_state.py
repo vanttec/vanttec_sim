6a2cf2209023aa16f508d116121e1840e2071401
Ivan5d <ivan.d557d@gmail.com>
2020-11-20 22:27:57 -0600

    Initial Commit

--- shooter_description/shooter_gazebo/scripts/model_state.py
+++ shooter_description/shooter_gazebo/scripts/model_state.py
@@ -6,4 +6,5 @@ from gazebo_msgs.msg import ModelState
 #Este debe ser publisher y el modelo tener un plugin, es decir convertirse en receptor
 
 def Model_State():
-    pub = rospy.Publisher("Pose_Torpedo_1", ModelState, ) 
+    pub = rospy.Publisher("Pose_Torpedo_1", ModelState, queue_size=10)
+     
