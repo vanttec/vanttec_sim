55bd45e7b31999659c816a39a195e78004261478
Ivan5d <ivan.d557d@gmail.com>
2021-04-15 12:45:11 -0500

    Subscribes image and publishes teleop

--- vtec_u3_description/scripts/teleop.py
+++ vtec_u3_description/scripts/teleop.py
@@ -2,109 +2,103 @@
 
 import rospy
 import keyboard 
-from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
 
 import sys, select, os
 if os.name == 'nt':
-  import msvcrt
+    import msvcrt
 else:
-  import tty, termios
+    import tty, termios
 
 status = 0
-
 msg = """
 
 Teleop vtec_u3 
 ---------------------------
 1) Lineal:
-    a = izquierda
-    d = derecha
-    w = enfrente
-    s = atras 
+    a = left
+    d = rights
+    w = forwards
+    s = backwards
 2) Angular:
-    i = yaw derecho
-    o = yaw izquierdo
+    i = right yaw 
+    o = left yaw
 3) Picture: 
     p = take picture
 
 CTRL-C to quit
 """
 
+#Diagrama general: camera ---> callback --[if]--> teleop 
 def getKey():
     if os.name == 'nt':
         return msvcrt.getch()
-
     tty.setraw(sys.stdin.fileno())
     rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
     if rlist:
         key = sys.stdin.read(1)
     else:
         key = ''
-
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
 #--------------------------------------------
-def Teleop():
-    #Publish: one topic Twist message type 
-    pub = rospy.Publisher('/teleop', Twist, queue_size=10)
-
-    #Initiates node
-    rospy.init_node('teleop_u3', anonymous=False)
-
-    #10 Hz
-    rate = rospy.Rate(10) 
+#Publisher
+def callback(data):
     v_lin = 0.8
     v_ang = 0.8
-    status = 0
+    twist=Twist()
 
-    while not rospy.is_shutdown():
+    key = getKey()
+    #Forwards
+    if key=='a':
+        twist.linear.y = v_lin 
+    #Right
+    elif key=='d':
+        twist.linear.y = -1*v_lin 
+    #Left
+    elif key=='w':
+        twist.linear.x = v_lin
+    #Backwards
+    elif key=='s':
+        twist.linear.x = -1*v_lin 
+    #z down
+    elif key=='l':
+        twist.linear.z = -1*v_ang
+    #z up
+    elif key=='k':
+        twist.angular.y = v_ang     
+    #Right yaw
+    elif key=='i':
+        twist.angular.z = v_ang
+    #Left yaw 
+    elif key=='o':
+        twist.angular.z = -1*v_ang
         
-        twist=Twist()
+    rospy.loginfo(key)
+    pub = rospy.Publisher('/teleop', Twist, queue_size=10)
+    rate = rospy.Rate(10)
+    pub.publish(twist)
+    rate.sleep()
 
-        key = getKey()
-        #enfrente
-        if key=='a':
-            twist.linear.y = v_lin 
-        #Right
-        elif key=='d':
-            twist.linear.y = -1*v_lin 
-        #izquierda
-        elif key=='w':
-            twist.linear.x = v_lin
-        #atras
-        elif key=='s':
-            twist.linear.x = -1*v_lin 
-        #z abajo
-        elif key=='l':
-            twist.linear.z = -1*v_ang
-        #z arriba
-        elif key=='k':
-            twist.angular.y = v_ang     
-        #Raw
-        elif key=='i':
-            twist.angular.z = v_ang
-        #Yaw izquierda
-        elif key=='o':
-            twist.angular.z = -1*v_ang
-        #This key is control+c for killing the terminal
-        if (key == '\x03'):
-            break
-        rospy.loginfo(key)
-        status = status + 1
-        
-        if status == 20 :
-            print (msg) 
-            status = 0
+def Image_Subscriber(msg, status):
+     #/frontr200/camera/color/image_raw [sensor_msgs/Image] 1 publisher
 
-        pub.publish(twist)
-        rate.sleep()
-   
+    rospy.init_node('Teleop', anonymous=True)
+    rospy.Subscriber("/frontr200/camera/color/image_raw", Image, callback) #Callback
+    rospy.spin()
+
+    #For menu terminal status  
+    status += 1
+    if status == 20:
+        print (msg) 
+        status = 0
+
 if __name__ == '__main__':
     if os.name != 'nt':
         settings = termios.tcgetattr(sys.stdin)
     try:
-        Teleop()
+        Image_Subscriber(msg,status)
     except rospy.ROSInterruptException:
         pass
