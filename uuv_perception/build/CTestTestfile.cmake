925a889420431fedb6badfb6f7ecc8471ba7285e
Glazzerino <fztmon@gmail.com>
2021-06-15 17:48:38 -0500

    repackaging

---
+++ uuv_perception/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/glazz/code/vanttec_uv_sim/uuv_perception/src
+# Build directory: /home/glazz/code/vanttec_uv_sim/uuv_perception/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(gate_detector)
---
+++ uuv_perception/build/gate_detector/CMakeFiles/gate_detector_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gate_detector_node.dir/src/gate_detector.cpp.o"
+  "/home/glazz/code/vanttec_uv_sim/uuv_perception/devel/lib/gate_detector/gate_detector_node.pdb"
+  "/home/glazz/code/vanttec_uv_sim/uuv_perception/devel/lib/gate_detector/gate_detector_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gate_detector_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
