46737236bf6d349a31c78e811a70555b2c0a3e2b
vSebas <sebas.martp@gmail.com>
2021-06-22 22:16:07 -0500

    Changed comments

--- uuv_perception/scripts/color_srv.py
+++ uuv_perception/scripts/color_srv.py
@@ -3,7 +3,7 @@
 import rospy
 import cv2
 import numpy as np
-from usv_perception.srv import color_id
+from uuv_perception.srv import color_id
 from cv_bridge import CvBridge, CvBridgeError
 
 bridge = CvBridge()
